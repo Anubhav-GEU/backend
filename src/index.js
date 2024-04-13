ed1028c719d376b03f100c0e80dc7ac87e0b5707
Anubhav-GEU <anubhav860102@gmail.com>
2024-04-13 22:33:05 +0530

    utils and middlewares

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config()
 import dotenv from "dotenv";
 import connectDB from './db/index.js';
+import { app } from "./app.js";
 
 
 dotenv.config({
@@ -33,4 +34,18 @@ const app = express();
 })()
 */
 
+
+// connectdb  is asynchronous so also it returns a promise
 connectDB()
+.then(() => {
+    app.on('error', (error) => {
+        console.error('ERROR: ', error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on PORT ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("\nDB CONNECTION FAILED \n : ", err);
+})
